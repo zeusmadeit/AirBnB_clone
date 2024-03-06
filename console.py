055b216cdcbe86b21e9371b4b83bae495d65c151
wesam-c <wesameldeeb15@gmail.com>
2024-03-06 11:58:28 +0400

    Update console.py
    modified

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 
 import cmd
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb)"
@@ -42,6 +43,89 @@ class HBNBCommand(cmd.Cmd):
             if len(args) < 2:
                 print("** instance id missing **")
                 return
+            obj_id = args[1]
+            key = "{}.{}".format(class_name, obj_id)
+            storage = FileStorage()
+            storage.reload()
+            all_objs = storage.all()
+            if key in all_objs:
+                print(all_objs[key])
+            else:
+                print("** no instance found **")
+        except NameError:
+            print("** class doesn't exist **")
+    
+    def do_destroy(self, arg):
+        """Delete an instance based on the class name and id."""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+        try:
+            class_name = args[0]
+            if len(args) < 2:
+                print("** instance id missing **")
+                return
+            obj_id = args[1]
+            key = "{}.{}".format(class_name, obj_id)
+            storage = FileStorage()
+            storage.reload()
+            all_objs = storage.all()
+            if key in all_objs:
+                del all_objs[key]
+                storage.save()
+            else:
+                print("** no instance found **")
+        except NameError:
+            print("** class doesn't exist **")
+
+    def do_all(self, arg):
+        """Print all string representations of all instances."""
+        args = arg.split()
+        storage = FileStorage()
+        storage.reload()
+        all_objs = storage.all()
+        if not args:
+            print([str(all_objs[key]) for key in all_objs])
+        else:
+            try:
+                class_name = args[0]
+                print([str(all_objs[key])
+                       for key in all_objs if key.startswith(class_name)])
+            except NameError:
+                print("** class doesn't exist **")
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id by adding or updating attribute."""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+        try:
+            class_name = args[0]
+            if len(args) < 2:
+                print("** instance id missing **")
+                return
+            obj_id = args[1]
+            key = "{}.{}".format(class_name, obj_id)
+            storage = FileStorage()
+            storage.reload()
+            all_objs = storage.all()
+            if key not in all_objs:
+                print("** no instance found **")
+                return
+            if len(args) < 3:
+                print("** attribute name missing **")
+                return
+            if len(args) < 4:
+                print("** value missing **")
+                return
+            attr_name = args[2]
+            attr_value = args[3]
+            setattr(all_objs[key], attr_name, attr_value)
+            all_objs[key].save()
+        except NameError:
+            print("** class doesn't exist **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
