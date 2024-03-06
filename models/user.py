a993aa9d2ae743be4697c4bbd6f7e6f9ca91884a
ZeusMadeIt <58306978+ZeusMadeIt@users.noreply.github.com>
2024-03-06 09:19:25 +0000

    user model created, will be edited on User branch

---
+++ models/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Write a class User that inherits from BaseModel
+"""
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """ The User class defines all attributes
+        and methods for managing a User.  
+    """
+    pass
